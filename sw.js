6210e48cfaf4389ef5a799c2a977797126c298b1
Alexandre Marcondes <alex@marcondesian.net>
2018-04-01 18:14:35 -0400

    First pass at debt collective overrides + demo page

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
 /* global workbox:false */
 
-self.importScripts('/assets/js/vendor/{fileName}')
-
-workbox.precaching.precacheAndRoute([])
+self.importScripts("/assets/js/vendor/{fileName}");
+workbox.precaching.precacheAndRoute([]);
